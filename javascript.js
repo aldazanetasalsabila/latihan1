b3e1fc949418b833425b04883ae36d9a964bba07
Doktaraa <e42242008@student.polije.ac.id>
2025-09-10 15:23:24 +0700

    latihan javascript

---
+++ javascript.js
@@ -0,0 +1,17 @@
+// alert ("Thank You")
+// confirm ("Alda Zaneta Salsabila")
+// prompt ("E42242008")
+// Date ("16 agustus 2006")
+
+// parseInt("pppppp")
+// setTimeout(aldyz,7000)
+
+// function foo() {
+// alert("Our function just ran!");
+// } foo(); a
+
+function alertArraySize(arr) {
+    alert(arr.length);
+}
+var test = [1,2,3,4,5];
+alertArraySize(test);
